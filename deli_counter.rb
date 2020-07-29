f4209558f941e1d04fea2a330c859345eac142f1
Evan Pavone <evanrpavone@gmail.com>
2020-07-29 16:46:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,3 +21,12 @@ def take_a_number(line, new_person)
 end
 
 take_a_number(katz_deli, "Fyvish")
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
+end
