5544e9e80491b1f0dcab098c7e8675aed5f64275
Evan Pavone <evanrpavone@gmail.com>
2020-07-29 16:20:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,7 @@ katz_deli = []
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  elsif array.length > 0
-    puts "The line is currently:"katz_deli
-
-
+  else
+    return ("The line is currently:" + katz_deli)
   end
 end
