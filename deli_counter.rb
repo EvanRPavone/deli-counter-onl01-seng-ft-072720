8189e76a17a98c9a07dd790dba6ec68196d98e9b
Evan Pavone <evanrpavone@gmail.com>
2020-07-29 16:45:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,12 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+line(katz_deli)
+
+def take_a_number(line, new_person)
+  line.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
+
+take_a_number(katz_deli, "Fyvish")
