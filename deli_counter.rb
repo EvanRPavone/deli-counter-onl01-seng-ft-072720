0afcba54190353596ec97b9cee26eb5ef5c0435f
Evan Pavone <evanrpavone@gmail.com>
2020-07-29 16:24:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 
 def line(katz_deli)
+  katz_deli.each do |name|
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-
+    counter+=1
+    puts "The line is currently: #{counter}. #{name}"
   end
 end
